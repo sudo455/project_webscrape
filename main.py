a995f73d26b71f8cc7bcf56ebeef7760cd62544f
sudo455 <git.Repurpose6456.Polish5090@dr3ff1c13nt.com>
2024-06-06 22:29:58 +0300

    documentation

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-"""
-TODO: Impliment a web scraping tool with Beautiful Soup or Scrapy and output them to json or csv
-site : quotes.toscrape.com
-site : books.toscrape.com
-"""
-
 try: # try to see if all the needed libraries are installed
 	from selenium import webdriver
 	from selenium.webdriver.common.by import By
@@ -14,36 +8,41 @@ except ImportError:
   print("Error: Some or one library is not installed. Please install them using pip:\n pip install -r requirements.txt")
   exit()
 
+#initialise web drivers
 driver_gbp_to_euro = webdriver.Firefox()
-driver_gbp_to_euro.get("https://www.exchange-rates.org/converter/gbp-eur")  # Replace with the path to your local HTML file
-
-time.sleep(2)
-
-gbp_euro = driver_gbp_to_euro.find_element(By.CSS_SELECTOR, '.to-rate').text
-
-driver_gbp_to_euro.quit()
-
 driver_quotes = webdriver.Firefox()
-driver_quotes.get("http://quotes.toscrape.com")
-
 driver_books = webdriver.Firefox()
-driver_books.get("https://books.toscrape.com/index.html")
 
-driver_quotes.find_element(By.LINK_TEXT, "Login").click()
+driver_gbp_to_euro.get("https://www.exchange-rates.org/converter/gbp-eur")  # live british pount to euro convertion website
 
 time.sleep(2)
+
+gbp_euro = driver_gbp_to_euro.find_element(By.CSS_SELECTOR, '.to-rate').text # get the value of euros per 1 British Pound(gbp)
+
+driver_gbp_to_euro.quit()# exit web driver because is not needed
+
+driver_quotes.get("http://quotes.toscrape.com") # scrape quotes pre loader
+driver_books.get("https://books.toscrape.com/index.html") # scrape books pre loader 
+
+##########################################################################
+# block to login to quotes to scrape
+driver_quotes.find_element(By.LINK_TEXT, "Login").click()
+time.sleep(2)
+
 driver_quotes.find_element(By.ID, "username").send_keys("admin")
 driver_quotes.find_element(By.ID, "password").send_keys("test")
 
 driver_quotes.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
+##########################################################################
 
-data_frame_quotes = pd.DataFrame(columns=["QUOTES", "AUTHORS"])
-page_quotes_to_scrape = 1
+data_frame_quotes = pd.DataFrame(columns=["QUOTES", "AUTHORS"]) # dataframe for quotes
+page_quotes_to_scrape = 1 # for user output 
 
 
-data_frame_books = pd.DataFrame(columns=["Title", "Star rating", "Price", "Stock availability"])
-page_books_to_scrape = 1
-
+data_frame_books = pd.DataFrame(columns=["Title", "Star rating", "Price", "Stock availability"]) # dataframe for the books
+page_books_to_scrape = 1 # for user output 
+############################################################
+# block to scrape the site: http://quotes.toscrape.com
 while True:
 
 	print(f"Page: {page_quotes_to_scrape} scraping...", )
@@ -66,6 +65,11 @@ while True:
 	except NoSuchElementException:
 		print("End of site quotes to scrape")
 		break
+############################################################
+
+
+############################################################
+# block to scrape the site: https://books.toscrape.com
 
 while True:
   products = driver_books.find_elements(By.CSS_SELECTOR, 'article.product_pod')
@@ -94,15 +98,18 @@ while True:
   except NoSuchElementException:
     print("End of site books to scrape")
     break
+############################################################
 
-driver_quotes.quit()
-driver_books.quit()
+driver_quotes.quit() 
+driver_books.quit() # safe exit of the drivers
 
 data_frame_quotes.index += 1
-print(data_frame_quotes)
-data_frame_quotes.to_csv(r'out.csv')
+data_frame_books.index += 1 # fix index of dataframes because the dataframe starts with 0 and not 1
 
 # Print the results
-data_frame_books.index += 1
+print(data_frame_quotes)
 print(data_frame_books)
+
+# write the output to 2 .csv files
+data_frame_quotes.to_csv(r'out.csv')
 data_frame_books.to_csv(r'out2.csv')
